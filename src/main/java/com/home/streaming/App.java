614552c376b3fc9fbd01b2e00e1aa9719de7d89f
Egor Goloshchapov <egor_goloshchapov@epam.com>
2021-02-15 17:55:07 +0100

    classes added

---
+++ src/main/java/com/home/streaming/App.java
@@ -0,0 +1,65 @@
+package com.home.streaming;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.log4j.Logger;
+
+import java.util.Optional;
+import java.util.Properties;
+
+public class App {
+
+  static final String DONE = "done";
+  private static final Logger log = Logger.getLogger(App.class);
+
+  public static void main(String[] args) {
+    log.warn("!!!!!!!!!!!!!!!!!!!Starting CONSUMER");
+    Properties config = new Properties();
+    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "ks-scaling-app-app-id");
+    config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
+      Optional.ofNullable(System.getenv("BOOTSTRAP_SERVERS_CONFIG")).orElse("localhost:9094")
+    );
+    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    config.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100);
+    config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10);
+
+    StreamsBuilder builder = new StreamsBuilder();
+
+    builder
+      .stream("inScalingTopic", Consumed.with(Serdes.String(), Serdes.String()))
+      .peek((key, value) -> {
+        if(!DONE.equals(value)) {
+          try {
+            Thread.sleep(10);
+            if (Long.valueOf(value.substring(3)) % 1000 == 0) {
+              log.info("Processed another 1000 messages");
+            }
+          } catch (InterruptedException e) {
+            log.error(e);
+          }
+        }
+      })
+      .filter((key, value) -> DONE.equals(value))
+      .peek((key, value) -> log.info("Done with key " + key))
+      .to("outScalingTopic", Produced.with(Serdes.String(), Serdes.String()))
+    ;
+
+    KafkaStreams streams = new KafkaStreams(builder.build(), config);
+    streams.start();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      try {
+        streams.close();
+        log.info("Stream stopped");
+      } catch (Exception exc) {
+        log.error("Got exception while executing shutdown hook: ", exc);
+      }
+    }));
+  }
+}
+
---
+++ src/main/java/com/home/streaming/Sender.java
@@ -0,0 +1,56 @@
+package com.home.streaming;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+
+public class Sender {
+
+  private static final Logger LOG = Logger.getLogger(Sender.class);
+
+  public static void main(String[] args) {
+    LOG.warn("!!!!!!!!!!!!!!!!!!!Starting SENDER");
+    Properties properties = new Properties();
+    properties.put("bootstrap.servers", "localhost:9094");
+    properties.put("acks", "all");
+    properties.put("retries", 0);
+    properties.put("batch.size", 16384);
+    properties.put("linger.ms", 1);
+    properties.put("buffer.memory", 33554432);
+
+    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+    Producer<String, String> producer = new KafkaProducer<>(properties);
+
+    List<String> keys = new ArrayList<>();
+    for(int i = 0; i < 30; i++) {
+      keys.add(UUID.randomUUID().toString());
+    }
+
+    LOG.info("Start");
+    for (String key : keys) {
+      for (int i = 0; i < 10000; i++) {
+        send(producer, key, "val" + i);
+      }
+      send(producer, key, App.DONE);
+    }
+
+    LOG.info("Closing Kafka Producer");
+    producer.close();
+  }
+
+  private static void send(Producer<String, String> producer, String key, String value) {
+    ProducerRecord<String, String> record = new ProducerRecord<>("inScalingTopic", key, value);
+    producer.send(record);
+  }
+
+}
